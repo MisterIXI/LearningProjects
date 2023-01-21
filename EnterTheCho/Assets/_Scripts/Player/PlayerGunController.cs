17fc5d6ea2a2fc4f9dcd9a60b7786b916311c723
Yannik <yannik.braendle@t-online.de>
2023-01-21 14:40:21 +0100

    script start

--- EnterTheCho/Assets/_Scripts/Player/PlayerGunController.cs
+++ EnterTheCho/Assets/_Scripts/Player/PlayerGunController.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
 
 public class PlayerGunController : MonoBehaviour
 {
     // control of the gun class and using them
+
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnDodgeInput(InputAction.CallbackContext context)
+    {
+
+    }
+    private void SubscribeToActions()
+    {
+        InputManager inputManager = RefManager.inputManager;
+        inputManager.OnMove += OnMoveInput;
+    }
+
+    private void UnSubscribeToActions()
+    {
+        // Unsubscribe to actions
+    }
+
+    private void OnDestroy() {
+        UnSubscribeToActions();
+    }
 }
